dfb63641cdbf218d39a1fc34df95343c0556378d
Aayushi Sharma <82430975+aayushisharma26@users.noreply.github.com>
2024-09-20 06:56:22 +0000

    Correct Code

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ import express from "express";
 const app =express();
 
 app.get("/",(res,req)=>{
-    res.setEncoding("Server is runing");
+    res.send("Server is runing");
 })
 
 app.listen(4000,()=>{
